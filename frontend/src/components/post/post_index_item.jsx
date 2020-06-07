493c66404ffa89a09af9969c7ef6db47f591b981
Patrick <patrick.yrkim@gmail.com>
2020-06-07 00:02:15 -0700

    Add delete post button to feed

--- frontend/src/components/post/post_index_item.jsx
+++ frontend/src/components/post/post_index_item.jsx
@@ -1,27 +1,45 @@
 import React from 'react';
 
 class PostIndexItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete() {
+        this.props.deletePostItem(this.props.match.params.postId)
+            .then(this.props.history.push('/posts'))
+    };
+
     render() {
         // table sorting
         
         return (
             <div>
-                <h3>{this.props.title}</h3>
-                <h3>{this.props.start}</h3>
-                <h3>{this.props.destination}</h3>
-                <h3>{this.props.time}</h3>
-                <h3>{this.props.description}</h3>
-                {/* <h3>{this.props.user}</h3> */}
-                {/* <table>
-                    <thead></thead>
-                    <tr>
-                        <th>{this.props.title}</th>
-                        <th>{this.props.start}</th>
-                        <th>{this.props.destination}</th>
-                        <th>{this.props.time}</th>
-                        <th>{this.props.description}</th>
-                    </tr>
-                </table> */}
+                <div>
+                    <h3>{this.props.title}</h3>
+                    <h3>{this.props.start}</h3>
+                    <h3>{this.props.destination}</h3>
+                    <h3>{this.props.time}</h3>
+                    <h3>{this.props.description}</h3>
+                    {/* <h3>{this.props.user}</h3> */}
+                    {/* <table>
+                        <thead></thead>
+                        <tr>
+                            <th>{this.props.title}</th>
+                            <th>{this.props.start}</th>
+                            <th>{this.props.destination}</th>
+                            <th>{this.props.time}</th>
+                            <th>{this.props.description}</th>
+                        </tr>
+                    </table> */}
+                </div>
+                <div>
+                    <div className="post-delete-button-container">
+                        <button className="delete-post" onClick={this.handleDelete}>Delete Post</button>
+                    </div>
+                </div>
             </div>
         );
     };
---
+++ frontend/src/components/post/post_index_item_container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import { fetchPost, deletePostItem } from '../../actions/post_actions';
+import PostIndexItem from './post';
+
+const mapStateToProps = (state) => {
+    return {
+        post: Object.values(state.post.all)
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchPost: () => dispatch(fetchPost()),
+        deletePostItem: (postId) => dispatch(deletePostItem(postId))
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(PostIndexItem);
