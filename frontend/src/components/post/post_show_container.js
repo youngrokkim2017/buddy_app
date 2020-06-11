32c0feeea35a91d36735f97fc7d94e351f1929d0
Patrick <patrick.yrkim@gmail.com>
2020-06-11 01:41:45 -0700

    Fix post action

--- frontend/src/components/post/post_show_container.js
+++ frontend/src/components/post/post_show_container.js
@@ -8,7 +8,7 @@ const mapStateToProps = (state, ownProps) => {
     //     currentUser: state.session.user,
     // };
 
-    // let postId = ownProps.match.params.postId;
+    let postId = ownProps.match.params.postId;
     // let postId = ownProps.match.params._id;
     // let postId = ownProps.match.params.id;
     // let postId = ownProps.id;
@@ -18,13 +18,17 @@ const mapStateToProps = (state, ownProps) => {
         currentUserId = state.session.user.id;
     };
 
+    let posts = Object.values(state.entities.posts);
+
     return {
         // post: state.post[postId],
         // post: state.entities.post[postId],
         currentUserId,
         // posts: Object.values(state.entities.posts.all),
-        posts: Object.values(state.entities.posts),
-        // post: state.entities.posts.all[ownProps.match.params.postId],
+        // posts: Object.values(state.entities.posts),
+        post: state.entities.posts[ownProps.match.params.postId],
+        postId,
+        posts,
     }
 };
 
