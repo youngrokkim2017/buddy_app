40014bcde3591e54fe58ae2b629181f7134a9801
Patrick <patrick.yrkim@gmail.com>
2020-07-23 23:58:20 -0700

    Add chat components

---
+++ frontend/src/components/chat/chat.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class Chat extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
---
+++ frontend/src/components/chat/chat_container.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import { fetchChats } from '../../actions/chat_actions';
+import Chat from './chat';
+import { fetchAUser } from '../../util/user_api_util';
+
+const mapStateToProps = (state) => {
+    return {
+        currentUser: state.session.user,
+        chats: state.entities.chats,
+        users: Object.values(state.entities.users),
+        // need sockets
+    }
+};
+
+const mapDispatchToProps = dispatch = {
+    fetchChats: postId => dispatch(fetchChats(postId)),
+    // need to fetch the users in the chat
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Chat);
