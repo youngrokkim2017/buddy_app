5438744da3801a5a478e007c1eb84a755fba20e9
Patrick <patrick.yrkim@gmail.com>
2020-08-01 22:36:37 -0700

    Update chat component with chat and socket actions

--- frontend/src/components/chat/chat_container.js
+++ frontend/src/components/chat/chat_container.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
-import { fetchChats } from '../../actions/chat_actions';
+import { fetchChats, fetchChatMembers } from '../../actions/chat_actions';
+import { receiveListener, receiveEmit, receiveRoom, receiveExitRoom } from '../../actions/socket_actions';
 import Chat from './chat';
 // import { fetchAUser } from '../../util/user_api_util';
 
@@ -9,13 +10,20 @@ const mapStateToProps = (state) => {
         chats: state.entities.chats,
         users: Object.values(state.entities.users),
         // need sockets
+        socket: state.entities.chats,
     }
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        fetchChats: postId => dispatch(fetchChats(postId))
-        // need to fetch the users in the chat
+        // chat actions
+        fetchChats: postId => dispatch(fetchChats(postId)),
+        fetchChatMembers: postId => dispatch(fetchChatMembers(postId)),
+        // socket actions
+        receiveListener: listener => dispatch(receiveListener(listener)),
+        receiveEmit: emit => dispatch(receiveEmit(emit)),
+        receiveRoom: room => dispatch(receiveRoom(room)),
+        receiveExitRoom: room => dispatch(receiveExitRoom(room)),
     }
 }
 
