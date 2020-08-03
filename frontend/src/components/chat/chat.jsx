6722dc30e4983ba919911db8429282d9cfa4d5a0
Patrick <patrick.yrkim@gmail.com>
2020-08-02 23:48:21 -0700

    Update lifecycle methods to fetch data

--- frontend/src/components/chat/chat.jsx
+++ frontend/src/components/chat/chat.jsx
@@ -2,9 +2,30 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 class Chat extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            chat: [],
+        };
+    }
+
+    componentDidMount() {
+        this.props.receiveRoom(this.props.match.params.postId);
+        this.props.fetchChatMembers(this.props.match.params.postId);
+        this.props.fetchChats(this.props.match.params.postId)
+            .then(res => {
+                this.setState({
+                    chat: this.props.chats.map(chat => (
+                        <div>
+                            <div>
+                                {chat.content}
+                            </div>
+                        </div>
+                    ))
+                })
+            })
+    }
 
     render() {
         return (
