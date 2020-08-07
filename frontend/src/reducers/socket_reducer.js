b4c4934e1c37ee2c05198386eaeef11870927903
Patrick <patrick.yrkim@gmail.com>
2020-08-06 22:30:07 -0700

    Update socket states

--- frontend/src/reducers/socket_reducer.js
+++ frontend/src/reducers/socket_reducer.js
@@ -1,25 +1,25 @@
 import { RECEIVE_LISTENER, RECEIVE_ROOM, RECEIVE_EMIT, RECEIVE_EXIT_ROOM } from '../actions/socket_actions';
-// const io = requre('socket.io-client');
-// let socket = process.env.NODE_ENV === 'production' ? io() : io('http://localhost:5000');
+const io = requre('socket.io-client');
+let socket = process.env.NODE_ENV === 'production' ? io() : io('http://localhost:5000');
 
 export const socketReducer = (state ={}, action) => {
     Object.freeze(state);
 
-    // let newState = Object.assign({}, state);
+    let newState = Object.assign({}, state);
 
     switch(action.type) {
         case RECEIVE_ROOM:
-            //
-            return //
+            socket.emit('joinRoom', action.room);
+            return newState;
         case RECEIVE_LISTENER:
-            //
-            return //
+            socket.on(action.listener.action, action.listener.callback);
+            return newState;
         case RECEIVE_EMIT:
-            //
-            return //
+            socket.emit(action.emit.action, action.emit.value);
+            return newState;
         case RECEIVE_EXIT_ROOM:
-            //
-            return //
+            socket.emit('exitRoom', action.room);
+            return newState;
         default:
             return state;
     }
