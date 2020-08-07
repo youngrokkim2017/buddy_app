6c1ca2dcb1722164414c7e4b25c82a8795cbe754
Patrick <patrick.yrkim@gmail.com>
2020-08-06 22:32:16 -0700

    Fix errors

--- frontend/src/reducers/socket_reducer.js
+++ frontend/src/reducers/socket_reducer.js
@@ -1,5 +1,5 @@
 import { RECEIVE_LISTENER, RECEIVE_ROOM, RECEIVE_EMIT, RECEIVE_EXIT_ROOM } from '../actions/socket_actions';
-const io = requre('socket.io-client');
+const io = require('socket.io-client');
 let socket = process.env.NODE_ENV === 'production' ? io() : io('http://localhost:5000');
 
 export const socketReducer = (state ={}, action) => {
