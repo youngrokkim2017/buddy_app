a4c684c27b44d8828a2632325fdc4a495c9d25d7
Patrick <patrick.yrkim@gmail.com>
2020-08-15 23:59:21 -0700

    Add search state

--- frontend/src/components/filter/search.jsx
+++ frontend/src/components/filter/search.jsx
@@ -3,12 +3,26 @@ import React from 'react';
 class Search extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            query: '',
+            results: {},
+            loading: false,
+            message: '',
+        }
     }
 
     render() {
         return (
-            <div>
-
+            <div className="search-container">
+                <label>
+                    <input
+                        type='text'
+                        value=''
+                        id='search-input'
+                        placeholder='Search'
+                    />
+                </label>
             </div>
         )
     }
