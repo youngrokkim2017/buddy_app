b2467a39a2826aecf02194a66fabc993301ac46b
Patrick <patrick.yrkim@gmail.com>
2020-09-05 00:31:49 -0700

    Update chat to request api route

--- frontend/src/util/chat_api_util.js
+++ frontend/src/util/chat_api_util.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const getChats = (postId) => {
-    return axios.get(`/api/posts/${postId}/chat`);
-    // return axios.get(`/api/requests/${postId}/chat`);
+    // return axios.get(`/api/posts/${postId}/chat`);
+    return axios.get(`/api/requests/${postId}/chat`);
 };
 
 export const getMembers = (postId) => {
     // debugger
-    return axios.get(`/api/posts/${postId}/chat/users`);
-    // return axios.get(`/api/requests/${postId}/chat/users`);
+    // return axios.get(`/api/posts/${postId}/chat/users`);
+    return axios.get(`/api/requests/${postId}/chat/users`);
 }
